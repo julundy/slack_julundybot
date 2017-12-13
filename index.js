850a43f1d1912ac00104f55e7f56b73f6c15ba67
Justin Lundy <justin.lundy@deutschinc.com>
2017-12-12 16:51:17 -0800

    Allowing port to be set by heroic

--- index.js
+++ index.js
@@ -1,30 +1,47 @@
 'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
 const app = express();
+const user = 'deutsch';
+const pass = 'necktie-hemp-hamster';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
     console.log('Express server listening on port %d in %s mode', 
     server.address().port, app.settings.env);
-})
+});
 
 app.get('/', (req, res) => {
-    let text = req.body.text;
-    
-    // Implement bot here
+    let env = req.query.text
+    let dispatcher0 = 'http://' + user + ':' + pass + '@vwweb-qa-' + env + '-dispatcher-000.vwusasite.com:80/dispatcher/invalidate.cache/';
+    let dispatcher1 = 'http://' + user + ':' + pass + '@vwweb-qa-' + env + '-dispatcher-001.vwusasite.com:80/dispatcher/invalidate.cache/';    
+    let text = 'Environment #' + env + ' is cleared!';
 
-    // checks if it's not a digit
-    // if (! /^|d+$/.test(q.text)) {
-    //     res.send('Enter an environment number: ###');
-    //     return;
-    // }
+    request
+        .get(dispatcher0)
+        .on('error', function (error, response) {
+            console.log('url:', dispatcher0);        
+            console.log('error:', error);
+            console.log('statusCode:', response && response.statusCode);
+            text = 'There is something wrong with: ' + dispatcher0; 
+    });
+    request
+        .get(dispatcher1)
+        .on('error', function (error, response) {
+            console.log('url:', dispatcher1);        
+            console.log('error:', error);
+            console.log('statusCode:', response && response.statusCode);
+            text = 'There is something wrong with: ' + dispatcher1; 
+    });
+
+    text = 'Environment #' + env + ' is cleared!';
 
     let data = {
         response_type: 'in_channel', //public to the channel
-        text: 'Command sent!'
+        text: text
     }
 
     res.json(data);
