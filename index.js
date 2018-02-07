442b4fa0548ee8df8803dee7277babfccf8e0105
VWVPN <vwvpn@LANDEU-NMXc3666.local>
2018-02-06 16:00:49 -0800

    Changing from express to botkit

--- index.js
+++ index.js
@@ -1,48 +1,120 @@
-'use strict';
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
-const app = express();
-const user = 'deutsch';
-const pass = 'necktie-hemp-hamster';
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ______    ______    ______   __  __    __    ______
+ /\  == \  /\  __ \  /\__  _\ /\ \/ /   /\ \  /\__  _\
+ \ \  __<  \ \ \/\ \ \/_/\ \/ \ \  _"-. \ \ \ \/_/\ \/
+ \ \_____\ \ \_____\   \ \_\  \ \_\ \_\ \ \_\   \ \_\
+ \/_____/  \/_____/    \/_/   \/_/\/_/  \/_/    \/_/
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-const server = app.listen(process.env.PORT || 3000, () => {
-    console.log('Express server listening on port %d in %s mode', 
-    server.address().port, app.settings.env);
+ This is a sample Slack Button application that provides a custom
+ Slash command.
+
+ This bot demonstrates many of the core features of Botkit:
+
+ *
+ * Authenticate users with Slack using OAuth
+ * Receive messages using the slash_command event
+ * Reply to Slash command both publicly and privately
+
+ # RUN THE BOT:
+
+ Create a Slack app. Make sure to configure at least one Slash command!
+
+ -> https://api.slack.com/applications/new
+
+ Run your bot from the command line:
+
+ clientId=<my client id> clientSecret=<my client secret> PORT=3000 node bot.js
+
+ Note: you can test your oauth authentication locally, but to use Slash commands
+ in Slack, the app must be hosted at a publicly reachable IP or host.
+
+
+ # EXTEND THE BOT:
+
+ Botkit is has many features for building cool and useful bots!
+
+ Read all about it here:
+
+ -> http://howdy.ai/botkit
+
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+/* Uses the slack button feature to offer a real time bot to multiple teams */
+var Botkit = require('botkit');
+
+if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.PORT || !process.env.VERIFICATION_TOKEN) {
+    console.log('Error: Specify CLIENT_ID, CLIENT_SECRET, VERIFICATION_TOKEN and PORT in environment');
+    process.exit(1);
+}
+
+var config = {}
+if (process.env.MONGOLAB_URI) {
+    var BotkitStorage = require('botkit-storage-mongo');
+    config = {
+        storage: BotkitStorage({mongoUri: process.env.MONGOLAB_URI}),
+    };
+} else {
+    config = {
+        json_file_store: './db_slackbutton_slash_command/',
+    };
+}
+
+var controller = Botkit.slackbot(config).configureSlackApp(
+    {
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        scopes: ['commands'],
+    }
+);
+
+controller.setupWebserver(process.env.PORT, function (err, webserver) {
+    controller.createWebhookEndpoints(controller.webserver);
+
+    controller.createOauthEndpoints(controller.webserver, function (err, req, res) {
+        if (err) {
+            res.status(500).send('ERROR: ' + err);
+        } else {
+            res.send('Success!');
+        }
+    });
 });
 
-app.get('/', (req, res) => {
-    let env = req.query.text
-    let dispatcher0 = 'http://' + user + ':' + pass + '@vwweb-qa-' + env + '-dispatcher-000.vwusasite.com:80/dispatcher/invalidate.cache';
-    let dispatcher1 = 'http://' + user + ':' + pass + '@vwweb-qa-' + env + '-dispatcher-001.vwusasite.com:80/dispatcher/invalidate.cache';    
-    let text = 'Environment #' + env + ' is cleared!';
 
-    request
-        .get(dispatcher0)
-        .on('error', function (error, response) {
-            console.log('url:', dispatcher0);        
-            console.log('error:', error);
-            console.log('statusCode:', response && response.statusCode);
-            text = 'There is something wrong with: ' + dispatcher0; 
-    });
-    request
-        .get(dispatcher1)
-        .on('error', function (error, response) {
-            console.log('url:', dispatcher1);        
-            console.log('error:', error);
-            console.log('statusCode:', response && response.statusCode);
-            text = 'There is something wrong with: ' + dispatcher1; 
-    });
+//
+// BEGIN EDITING HERE!
+//
 
-    text = 'Environment #' + env + ' is cleared!';
+controller.on('slash_command', function (slashCommand, message) {
 
-    let data = {
-        response_type: 'in_channel', //public to the channel
-        text: text
+    switch (message.command) {
+        case "/echo": //handle the `/echo` slash command. We might have others assigned to this app too!
+            // The rules are simple: If there is no text following the command, treat it as though they had requested "help"
+            // Otherwise just echo back to them what they sent us.
+
+            // but first, let's make sure the token matches!
+            if (message.token !== process.env.VERIFICATION_TOKEN) return; //just ignore it.
+
+            // if no text was supplied, treat it as a help command
+            if (message.text === "" || message.text === "help") {
+                slashCommand.replyPrivate(message,
+                    "I echo back what you tell me. " +
+                    "Try typing `/echo hello` to see.");
+                return;
+            }
+
+            // If we made it here, just echo what the user typed back at them
+            //TODO You do it!
+            slashCommand.replyPublic(message, "1", function() {
+                slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
+            });
+
+            break;
+        default:
+            slashCommand.replyPublic(message, "I'm afraid I don't know how to " + message.command + " yet.");
+
     }
 
-    res.json(data);
 })
+;
+
